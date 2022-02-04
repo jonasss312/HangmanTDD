2cf1b6cb31838ce1d2d9fd512238e0246a03d9f2
jonbal3 <MegstuKebabus4>
2022-02-04 04:32:07 -0500

    [REFACTOR]renamed-file-names-to-match-class-names

--- Backend/src/gateway/api/WordsGateway.ts
+++ Backend/src/gateway/api/WordsGateway.ts
@@ -1,3 +1,3 @@
-export default interface GamesGatewayWordsGateway {
+export default interface WordsGateway {
     loadWord(): string;
 }
---
+++ Backend/src/gateway/implementation/GamesStorage.test.ts
@@ -0,0 +1,24 @@
+import { GamesStorage } from './GamesStorage'
+import { Game } from '../../domain/Game'
+
+let storage = new GamesStorage();
+const game = new Game(1, [], [], "");
+
+describe("GamesStorage", () => {
+  test("Can add game", () => {
+    storage.addGame(game);
+    expect(storage.getGames()).toContain(game);
+  });
+
+  test("Can get game by id", () => {
+    const foundGame = storage.getGame(1);
+    expect(foundGame).toEqual(game);
+  });
+
+  test("Can upsert game by id", () => {
+    storage.addGame(game);
+    const updatedGame = new Game(1, ["a"], ["b"], "cvbn");
+    storage.upsertGame(1, updatedGame);
+    expect(storage.getGame(1)).toEqual(updatedGame);
+  });
+});
