94c87281055b349d154ec72738f0fab949210767
jonbal3 <MegstuKebabus4>
2022-02-04 07:55:21 -0500

    [REFACTOR]c-d2b-converter-prettier

--- Backend/src/usecase/implementation/GameD2BConverter.ts
+++ Backend/src/usecase/implementation/GameD2BConverter.ts
@@ -3,6 +3,10 @@ import { BoundaryGame } from "../model/BoundaryGame";
 
 export class GameD2BConverter {
     convert(game: Game): BoundaryGame {
+        return this.createGameBoundary(game);
+    }
+
+    private createGameBoundary(game: Game): BoundaryGame{
         return new BoundaryGame(
             game.getId(),
             game.getGuessedLetters(),
