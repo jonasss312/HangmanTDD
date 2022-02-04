13a28f7f7b9ce8580452706dac4c353bfca0e85f
jonbal3 <MegstuKebabus4>
2022-02-04 04:29:36 -0500

    [REFACTOR]game-interactor

---
+++ Backend/src/usecase/implementation/CreateGameInteractor.test.ts
@@ -0,0 +1,52 @@
+import {mock, MockProxy} from 'jest-mock-extended';
+
+import { CreateGameInteractor } from './CreateGameInteractor'
+import { BoundaryGame } from '../model/BoundaryGame';
+import { Game } from '../../domain/Game';
+
+import WordsGateway from '../../gateway/api/WordsGateway'
+import GamesGateway from '../../gateway/api/GamesGateway'
+
+const WORDS_GW: MockProxy<WordsGateway> = mock<WordsGateway>();
+const GAMES_GW: MockProxy<GamesGateway> = mock<GamesGateway>();
+
+const gameCreator = new CreateGameInteractor(WORDS_GW, GAMES_GW);
+
+describe("GameCreator", () => {
+    test("Can create game", () => {
+        const id = 1
+        const testWord = "TEST"
+        const hiddenTestWord = "####"
+
+        WORDS_GW.loadWord.mockReturnValue(testWord)
+        GAMES_GW.generateId.mockReturnValue(id)
+
+        const createdGame = gameCreator.createGame()
+
+        const isGameBoundary = createdGame instanceof BoundaryGame;
+        const newGame = GAMES_GW.addGame.mock.calls[0][0];
+        
+        expect(WORDS_GW.loadWord).toHaveBeenCalled();
+        expect(GAMES_GW.generateId).toHaveBeenCalled();
+
+        assertCreatedGameForGateway(newGame, id, testWord);
+
+        assertCreatedGameBoundary(createdGame, id, hiddenTestWord);
+        
+        expect(isGameBoundary).toEqual(true);
+    });
+
+    function assertCreatedGameForGateway(game: Game, id:number, word: string){
+        expect(game.getId()).toEqual(id)
+        expect(game.getGuessedLetters()).toEqual([])
+        expect(game.getWrongLetters()).toEqual([])
+        expect(game.getWord()).toEqual(word);
+    }
+
+    function assertCreatedGameBoundary(game: BoundaryGame, id:number, word: string){
+        expect(game.getId()).toEqual(id)
+        expect(game.getGuessedLetters()).toEqual([])
+        expect(game.getWrongLetters()).toEqual([])
+        expect(game.getHiddenWord()).toEqual(word);
+    }
+});
