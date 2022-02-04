1fe9146092f553f2cf104ce4d38ba2b697381b0c
jonbal3 <MegstuKebabus4>
2022-02-04 04:24:31 -0500

    [REFACTOR]g-d2b-c-test

--- Backend/src/usecase/implementation/GameD2BConverter.test.ts
+++ Backend/src/usecase/implementation/GameD2BConverter.test.ts
@@ -2,12 +2,16 @@ import { GameD2BConverter } from '../implementation/GameD2BConverter'
 import { Game } from '../../domain/Game'
 import { BoundaryGame } from '../model/BoundaryGame';
 
-const GAME_D2B_CONVERTER = new GameD2BConverter();
+let gameD2BConverter: GameD2BConverter;
+
+beforeEach(() => {
+    gameD2BConverter = new GameD2BConverter();
+})
 
 describe("GameCreator", () => {
-    test("Can trasnfer attributes", () => {
+    test("Can transfer attributes", () => {
         const game = new Game(1, ["a", "b"], ["c", "d"], "test");
-        const boudaryGame : BoundaryGame = GAME_D2B_CONVERTER.convert(game);
+        const boudaryGame : BoundaryGame = gameD2BConverter.convert(game);
 
         expect(boudaryGame.getId()).toEqual(game.getId())
         expect(boudaryGame.getGuessedLetters()).toEqual(game.getGuessedLetters())
