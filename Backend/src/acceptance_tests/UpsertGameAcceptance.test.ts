51e8f412dbced04caaf21ba698d6f29b84ceb9a3
jonbal3 <MegstuKebabus4>
2022-02-10 08:21:01 -0500

    [REFACTOR]fixed-format-upsert-route

--- Backend/src/acceptance_tests/UpsertGameAcceptance.test.ts
+++ Backend/src/acceptance_tests/UpsertGameAcceptance.test.ts
@@ -5,20 +5,20 @@ import { performSetup } from "./AcceptanceSetup";
 import { RestGame } from "../rest/models/RestGame";
 
 describe("UpsertGameAcceptance", () => {
-  let server: Express;
+    let server: Express;
 
-  beforeEach(() => {
-    server = performSetup();
-  })
+    beforeEach(() => {
+        server = performSetup();
+    })
 
-  it("PATCH upsert game route API request", async () => {
-      const gameId = 5;
-    const expectedGame = new RestGame(gameId, ["T"], ["B"], "T##T", 2, "IN_PROGRESS");
-    const requestingRestGameUpdate = new RestGameUpdate(gameId, "T");
+    it("PATCH upsert game route API request", async () => {
+        const gameId = 5;
+        const expectedGame = new RestGame(gameId, ["T"], ["B"], "T##T", 2, "IN_PROGRESS");
+        const requestingRestGameUpdate = new RestGameUpdate(gameId, "T");
 
-    const result = await request(server).patch("/api/games/5").send(requestingRestGameUpdate);
+        const result = await request(server).patch("/api/games/5").send(requestingRestGameUpdate);
 
-    expect(result.body).toEqual(expectedGame);
-    expect(result.statusCode).toEqual(200);
-  });
+        expect(result.body).toEqual(expectedGame);
+        expect(result.statusCode).toEqual(200);
+    });
 });
