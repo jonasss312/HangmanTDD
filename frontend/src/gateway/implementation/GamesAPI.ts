f4c79830924f9414fe95c21364a7981f6cae7355
jonbal3 <MegstuKebabus4>
2022-02-11 12:48:14 +0200

    [feature]walking-sceleton

---
+++ frontend/src/gateway/implementation/GamesAPI.ts
@@ -0,0 +1,24 @@
+import { Game } from "domain/Game";
+import { GamesGateway } from "gateway/api/GamesGateway";
+import { map, Observable } from "rxjs";
+import { ajax } from "rxjs/ajax";
+
+export class GamesAPI implements GamesGateway {
+  private readonly connectionString: string;
+
+  constructor() {
+    this.connectionString = "http://localhost:8080/api/games";
+  }
+
+  createGame(): Observable<Game | undefined> {
+    let game: Game | undefined;
+    const subscription = ajax
+      .post<Game | undefined>(this.connectionString)
+      .pipe(map((data) => data.response))
+      .subscribe((data) => (game = data));
+    subscription.unsubscribe();
+    return new Observable<Game | undefined>((subscriber) => {
+      subscriber.next(game);
+    });
+  }
+}
