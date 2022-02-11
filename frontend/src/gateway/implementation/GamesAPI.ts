0905f86c2f601d6899e504ca9bda471131fb5f43
jonbal3 <MegstuKebabus4>
2022-02-11 12:50:29 +0200

    [REFACTOR]connection-string

--- frontend/src/gateway/implementation/GamesAPI.ts
+++ frontend/src/gateway/implementation/GamesAPI.ts
@@ -4,21 +4,20 @@ import { map, Observable } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
 export class GamesAPI implements GamesGateway {
-  private readonly connectionString: string;
+    private readonly connectionString: string;
 
-  constructor() {
-    this.connectionString = "http://localhost:8080/api/games";
-  }
+    constructor(connectionString : string) {
+        this.connectionString = connectionString;
+    }
 
-  createGame(): Observable<Game | undefined> {
-    let game: Game | undefined;
-    const subscription = ajax
-      .post<Game | undefined>(this.connectionString)
-      .pipe(map((data) => data.response))
-      .subscribe((data) => (game = data));
-    subscription.unsubscribe();
-    return new Observable<Game | undefined>((subscriber) => {
-      subscriber.next(game);
-    });
-  }
+    createGame(): Observable<Game | undefined> {
+        let game: Game | undefined;
+        const subscription = ajax
+            .post<Game | undefined>(this.connectionString)
+            .pipe(map((data) => data.response))
+            .subscribe((data) => (game = data));
+        subscription.unsubscribe();
+        return new Observable<Game | undefined>(subscriber => {
+            subscriber.next(game)});
+    }
 }
