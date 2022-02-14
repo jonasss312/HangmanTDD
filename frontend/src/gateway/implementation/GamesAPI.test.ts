57b8e6f770248791b49bb1f988bc5e4e4abb360c
jonbal3 <MegstuKebabus4>
2022-02-14 12:42:55 +0200

    [REFACTOR]adapted-create-game-api-to-use-obsrv-cst

--- frontend/src/gateway/implementation/GamesAPI.test.ts
+++ frontend/src/gateway/implementation/GamesAPI.test.ts
@@ -4,6 +4,7 @@ import { mock, MockProxy } from "jest-mock-extended";
 import { Observable, of } from "rxjs";
 import { GamesAPI } from "./GamesAPI";
 import { CreateGameRoute } from "../../constant/RestConstants";
+import { getObserverTemplate } from "../../constant/getObserverTemplate";
 
 describe("GamesAPI", () => {
   let gamesAPI: GamesAPI;
@@ -14,16 +15,19 @@ describe("GamesAPI", () => {
     gamesAPI = new GamesAPI(client);
   });
 
-  it("Can get new game", () => {
+  it("Can get new game", (done) => {
     const newGame = new Game(1, [], [], "____", 0, "IN_PROGRESS");
 
     client.post.mockReturnValue(of(newGame));
 
     const observableGame: Observable<Game> = gamesAPI.createGame();
 
-    observableGame.subscribe((game) => {
+    const observer = getObserverTemplate(done);
+    observer.next = (game) => {
       expect(client.post).toBeCalledWith(CreateGameRoute);
       expect(game).toEqual(newGame);
-    });
+    };
+
+    observableGame.subscribe(observer);
   });
 });
