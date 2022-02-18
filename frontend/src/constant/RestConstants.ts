9639a2bda0be3eaa43af3a3e5ac7121bc316845b
jonbal3 <MegstuKebabus4>
2022-02-18 15:15:15 +0200

    [REFACTOR]rename-api-domain

--- frontend/src/constant/RestConstants.ts
+++ frontend/src/constant/RestConstants.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL: string = process.env.REACT_APP_DOMAIN + "/";
+export const SERVER_URL: string = process.env.REACT_APP_API_URL + "/";
 
 export const GAMES_PATH: string = "api/games";
