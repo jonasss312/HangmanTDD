e780050ea7787fdec49f303751245d29b7392fa1
jonbal3 <MegstuKebabus4>
2022-02-21 10:34:49 +0200

    [FEATURE]guess-letter-use-case

--- frontend/src/gateway/api/GamesGateway.ts
+++ frontend/src/gateway/api/GamesGateway.ts
@@ -1,6 +1,8 @@
 import { Game } from "domain/Game";
+import { Guess } from "domain/Guess";
 import { Observable } from "rxjs";
 
 export interface GamesGateway {
   createGame(): Observable<Game>;
+  guessLetter(guess: Guess): Observable<Game>;
 }
