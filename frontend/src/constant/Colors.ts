96880cb8c675ace6433362ffa76d6609aab13e23
jonbal3 <MegstuKebabus4>
2022-02-23 16:02:48 +0200

    [REFACTOR]use-color

---
+++ frontend/src/constant/Colors.ts
@@ -0,0 +1,1 @@
+export const colors = ["#33eaff", "#9cffe3"];
--- frontend/src/view/component/useColorChange.tsx
+++ frontend/src/view/component/useColorChange.tsx
@@ -1,15 +1,15 @@
+import { colors } from "constant/Colors";
 import { useEffect } from "react";
 import { useState } from "react";
 
 export const useColorChange = (): string => {
-  const colors = ["#33eaff", "#9cffe3"];
   const [colorState, setState] = useState(colors[0]);
 
   let index = 1;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setState(colors[index++ % 2]);
+      setState(colors[index++ % colors.length]);
     }, 800);
 
     return () => clearInterval(interval);
